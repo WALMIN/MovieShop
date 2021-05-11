cf8d178118a985d681879be75697d4c6aa403f22
Victor Bergene <v@walmin.com>
2021-05-11 18:05:27 +0200

    Added a navigationbar & routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import NavigationBar from './components/NavigationBar';
+import Varukorg from './components/Varukorg';
+import { Route, HashRouter as Router, Link, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+    <Router basename="/">
+      <div className="App">
+        <main>
+        <Switch>
+          <Route exact path="/cart"> <Varukorg /> </Route>
+        </Switch>
+        </main>
+        <footer>
+          <NavigationBar />
+        </footer>
+      </div>
+    </Router>
   );
 }
 
