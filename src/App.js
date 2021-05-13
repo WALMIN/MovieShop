7a8541f37d312f71fa0196864eac290bb0c7a16a
nicolelopezfeliz <nicole.lopez@live.se>
2021-05-13 15:31:23 +0200

    Gets API with redux

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import MovieList from './components/MovieList';
+import React, {useState, useEffect} from 'react';
+import {actions} from './features/movieList'
+import { useSelector } from 'react-redux';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <h1>Movie Shop</h1>
       </header>
+
+      <main>
+        <MovieList />
+        <TestMovieList />
+      </main>
     </div>
   );
 }
+
+const TestMovieList = () => {
+  const apiList = useSelector(state => state.movieList.fact);
+
+  if(apiList === null) {
+    return (
+      <div>Ingen laddad lista, vänta.....</div>
+    )
+  } else {
+    return (
+      <div style={{background: '#f3f3f3'}}>
+        {apiList.map((item) => (
+          <p>{item.id} {item.name}</p>
+        ))}
+      </div>
+    )
+
+  }
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,9 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import rootReducer from './features/rootReducer';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+
+//vår store
+const store = configureStore({
+  //funktion som konfigurerar vår store 
+  reducer : rootReducer
+});
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
