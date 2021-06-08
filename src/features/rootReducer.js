c7ffbde39460d16347bad8bcbca69e631ee41a42
ud <maab970514@student.jenseneducation.se>
2021-06-08 13:31:32 +0200

    changed redux to localstorage and created Add to to favourite function

--- src/features/rootReducer.js
+++ src/features/rootReducer.js
@@ -1,11 +1,9 @@
 import { combineReducers } from "redux";
 import { reducer as movieListReducer } from './movieList';
-import { reducer as favouriteListReducer} from './favouriteList'
 import { reducer as cartReducer} from './cart'
 
 const rootReducer = combineReducers({
     movieList: movieListReducer,
-    favouriteList: favouriteListReducer,
     cart: cartReducer
 });
 
